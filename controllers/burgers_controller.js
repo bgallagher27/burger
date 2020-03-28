8e1a6a864ad081ee3598390d8b4efc9b5883e426
Brendan Gallagher <bpgallagher27@gmail.com>
2020-03-28 19:16:20 -0400

    created controller

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.router();
+const burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+        const burgerObj = {
+            burger: data
+        };
+        console.log(burgerObj);
+        res.render("index", burgerObj);
+    });
+});
+
+router.post("/api/burgers", function(req, res) {
+    burger.insertOne([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function(result) {
+        res.json(result)
+    });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+    const condition = "id = " + req.params.id;
+    burger.updateOne({
+        devoured: req.body.devoured
+    }, condition, function(result) {
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+          } else {
+            res.status(200).end();
+          };      
+    });
+});
+
+module.exports = router;
