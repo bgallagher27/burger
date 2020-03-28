9b39b76dfb8046b3bf2bece79919d6cbe70b27fd
Brendan Gallagher <bpgallagher27@gmail.com>
2020-03-28 16:23:26 -0400

    updated seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Burger", "false");
-INSERT INTO burgers (burger_name, devoured) VALUES ("Double Cheese Burger", "false");
-INSERT INTO burgers (burger_name, devoured) VALUES ("BBQ Burger", "false");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Cheese Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("BBQ Burger", FALSE);
