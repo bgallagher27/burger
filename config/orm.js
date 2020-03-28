e87ddc68772e620710ba71aac6a0b05774c9bef8
Brendan Gallagher <bpgallagher27@gmail.com>
2020-03-28 16:54:05 -0400

    created orm functions

--- config/orm.js
+++ config/orm.js
@@ -1,11 +1,29 @@
 const connection = require("./connection.js");
 
 const orm = {
-    selectAll: function() {
+    selectAll: function(cb) {
         const queryString = "SELECT * FROM burgers";
         connection.query(queryString, function(err, result) {
             if (err) throw err;
-      console.log(result);
+      cb(result);
         });
     },
+
+    insertOne: function(newBurger, cb) {
+        const queryString = "INSERT INTO burgers (burger_name, devoured) VALUES (?, FALSE)";
+        connection.query(queryString, [newBurger], function(err, result) {
+            if (err) throw err;
+      cb(result);
+        });
+    },
+
+    updateOne: function(devouredBurger, cb) {
+        const queryString = "UPDATE burgers"
+        queryString += "SET devoured = TRUE"
+        queryString += "WHERE id = " + devouredBurger;
+        connection.query(queryString, function(err, result) {
+            if (err) throw err;
+      cb(result);
+        });
+    }
 };
