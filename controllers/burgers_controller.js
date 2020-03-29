ba272d0228ba77fb817ecc8049444bbabe2fbdb2
Brendan Gallagher <bpgallagher27@gmail.com>
2020-03-29 08:08:03 -0400

    fixed express router syntax

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.router();
+const router = express.Router();
 const burger = require("../models/burger.js");
 
 router.get("/", function(req, res) {
